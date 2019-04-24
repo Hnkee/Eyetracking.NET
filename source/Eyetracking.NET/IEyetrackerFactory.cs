5132b09c8174c2848baa6b5ad23d8ee4fc93329b
Hnkee Ndersson <hnkee.ndersson@gmail.com>
2019-04-24 06:44:35 +0200

    Removed comments from factory

--- source/Eyetracking.NET/IEyetrackerFactory.cs
+++ source/Eyetracking.NET/IEyetrackerFactory.cs
@@ -1,20 +1,9 @@
 ﻿namespace Eyetracking.NET
 {
-    /// <summary>
-    /// Responsible for creating the eyetracker
-    /// </summary>
     public interface IEyetrackerFactory
     {
-        /// <summary>
-        /// Creates a new eyetracker
-        /// </summary>
-        /// <returns>IEeyetracker instance</returns>
         IEyetracker Create();
 
-        /// <summary>
-        /// Creates a new 
-        /// </summary>
-        /// <returns></returns>
         IEyetrackerVr CreateVR();
 
         bool CanCreateEyetracker { get; }
